67903ef7dd8df34f1fb5e7f4021481cc4ea54ff3
= <feluubotta685@gmail.com>
2026-06-02 22:47:14 -0300

    sandbox/main.cpp - Se expanden vértices y se agregan indices para dibujar el plano

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -3,6 +3,7 @@
 #include <efengine/renderer/Buffer.h>
 #include <efengine/renderer/VertexArray.h>
 #include <efengine/renderer/VertexLayout.h>
+#include <efengine/renderer/IndexBuffer.h>
 #include <efengine/core/Log.h>
 #include <efengine/core/Types.h>
 
@@ -30,7 +31,7 @@ void main() {
 int main() {
     using namespace efengine;
 
-    EF_LOG_INFO("=== efengine Sandbox: Fase 2 (Hello Triangle) ===");
+    EF_LOG_INFO("=== efengine Sandbox: Fase 3 (Plano) ===");
 
     application::Application app;
     platform::Window&   window = app.GetWindow();
@@ -43,17 +44,22 @@ int main() {
     }
 
     f32 vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,   // 0: inferior-izquierda
+        0.5f, -0.5f, 0.0f,   // 1: inferior-derecha
+        0.5f,  0.5f, 0.0f,   // 2: superior-derecha
+        -0.5f,  0.5f, 0.0f,   // 3: superior-izquierda
     };
 
+    // 4 vertices vec3
+    u32 indices[] = { 0, 1, 2, 2, 3, 0 };
+    
     renderer::Buffer       vbo(vertices, sizeof(vertices));
     renderer::VertexLayout layout;
     layout.Push(renderer::ShaderDataType::Float3);   // posición
 
     renderer::VertexArray va;
     va.AddVertexBuffer(std::move(vbo), layout);
+    va.SetIndexBuffer(renderer::IndexBuffer(indices, 6)); // Se mueve el EBO al VAO, el VAO toma propiedad del recurso
 
     while (!window.ShouldClose()) {
         window.PollEvents();
