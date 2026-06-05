1be7e7fcb16897dd87c9111f1fa80aaf6f6fdccb
= <feluubotta685@gmail.com>
2026-06-04 22:53:25 -0300

    efengine/scene - Definición de la clase Camera

---
+++ src/efengine/scene/Camera.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <efengine/core/Types.h>
+#include <glm/glm.hpp>
+
+namespace efengine {
+namespace scene {
+    class Camera {
+        public:
+        glm::mat4 ViewMatrix() const;
+        glm::mat4 ProjectionMatrix() const;
+        const glm::vec3& Position() const;
+
+        void LookAt(const glm::vec3& position, const glm::vec3& target, const glm::vec3& up = glm::vec3(0.0f, 1.0f, 0.0f));
+        void SetAspect(f32 aspect);
+
+
+        private:
+            glm::vec3 m_position { 0.0f, 0.0f, 1.5f };
+            glm::vec3 m_target   { 0.0f, 0.0f, 0.0f };
+            glm::vec3 m_up       { 0.0f, 1.0f, 0.0f };
+            f32 m_fov    = 45.0f;
+            f32 m_aspect = 1.0f;
+            f32 m_near   = 0.1f;
+            f32 m_far    = 100.0f;
+    };
+}
+}
