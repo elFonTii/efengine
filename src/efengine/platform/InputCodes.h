735799d489022b42e9541b7f6939dc2a573d51da
= <feluubotta685@gmail.com>
2026-06-09 21:46:48 -0300

    platform/InputCodes - Se agrega clase con códigos ASCII para el input!

---
+++ src/efengine/platform/InputCodes.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <efengine/core/Types.h>
+
+namespace efengine {
+namespace platform {
+    enum class Key : i32 {
+        // ASCII
+        Escape  = 256,
+        Space   = 32,
+
+        Down    = 264,
+        Up      = 265,
+        Left    = 263,
+        Right   = 262,
+        
+        W       = 87,
+        A       = 65,
+        S       = 83,
+        D       = 68,
+    };
+
+    enum class MouseButton : i32 {
+        Left = 0,
+        Right = 1,
+        Middle = 2,
+    };
+}
+}
