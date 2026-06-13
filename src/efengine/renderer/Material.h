2a405b9595579dd67efb83d4b3faa5ce1d709117
= <feluubotta685@gmail.com>
2026-06-13 12:24:29 -0300

    renderer/Material - Se define contrato de clase Material

---
+++ src/efengine/renderer/Material.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <efengine/core/Types.h>
+#include <glm/glm.hpp>
+    // forward dec
+    class Shader; 
+    class Texture;
+    
+// material no posee, solo contiene observadores para shaders y texturas.
+namespace efengine {
+namespace renderer {
+    class Material {
+        public:
+            explicit Material(const Shader* shader) {};
+
+            // setters
+            void SetAlbedoMap(const Texture* texture);
+            void SetNormalMap(const Texture* texture);
+            void SetAOMap(const Texture* texture);
+            void SetRoughnessMap(const Texture* texture);
+            void SetMetallicMap(const Texture* texture);
+            void SetHeightMap(const Texture* texture);
+
+            // escalares para el muestreo de cada tex
+            glm::vec3 albedoTint = glm::vec3(1.0f);
+            f32 metallic = 0.0f;
+            f32 roughness = 0.5f;
+            f32 aoStrength = 1.0f;
+            f32 heightScale = 0.05f;
+
+            void Bind() const;
+
+        private:
+            const Shader* m_shader;
+            
+            const Texture* m_albedo    = null;
+            const Texture* m_normal    = null;
+            const Texture* m_ao        = null;
+            const Texture* m_roughness = null;
+            const Texture* m_metallic  = null;
+            const Texture* m_height    = null;
+    };
+}
+}
