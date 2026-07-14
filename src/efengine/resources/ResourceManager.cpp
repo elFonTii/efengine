f56d172dafae589406298e209e6a517c954d3805
= <feluubotta685@gmail.com>
2026-07-14 19:28:13 -0300

    resources/ResourceManager - Declaración de clase ResourceManager y método GetModel

--- src/efengine/resources/ResourceManager.cpp
+++ src/efengine/resources/ResourceManager.cpp
@@ -1,1 +1,27 @@
 #include "ResourceManager.h"
+#include <efengine/resources/ModelLoader.h>
+#include <efengine/core/Assert.h>
+#include <utility>
+
+namespace efengine {
+namespace resources {
+    renderer::Model* ResourceManager::GetModel(const char* path){
+        EF_ASSERT(path != null, "ResourceManager::GetModel: Intentando acceder a un directorio nulo");
+        
+        if (auto it = m_models.find(path); it != m_models.end()) {
+            // found
+            return &it->second;
+        } else {
+            // not found, not cached -> load the model.
+            auto result = resources::ModelLoader::Load(path);
+            if(!result) return null;
+
+            auto [inserted, ok] = m_models.emplace(path, std::move(*result));
+            EF_ASSERT(ok, "ResourceManager::GetModel: emplace no insertó tras un miss");
+
+            // insertó, retornar
+            return &inserted->second;
+        }
+    }
+}
+}
--- src/efengine/resources/ResourceManager.h
+++ src/efengine/resources/ResourceManager.h
@@ -1,9 +1,17 @@
 #pragma once
+#include <efengine/renderer/Model.h>
+#include <efengine/core/Types.h>
+#include <unordered_map>
+#include <string>
 
 namespace efengine {
-namespace resources{
+namespace resources {
     class ResourceManager {
-        
+        public:
+            renderer::Model* GetModel(const char* path);
+            
+        private:
+            std::unordered_map<std::string, renderer::Model> m_models;
     };
 }
 }
