8ba7ce5568844f221554cec2c2c61d2814449b17
= <feluubotta685@gmail.com>
2026-07-14 19:31:49 -0300

    tests/resources - Test rojo de ResourceManager (getTexture)

--- tests/resources/ResourceManager.test.cpp
+++ tests/resources/ResourceManager.test.cpp
@@ -6,3 +6,9 @@ TEST_CASE("ResourceManager::GetModel ruta inexistente -> null") {
     efengine::resources::ResourceManager rm;
     CHECK(rm.GetModel("assets/no_existe_xyz.fbx") == nullptr);
 }
+
+TEST_CASE("ResourceManager::GetTexture ruta inexistente -> null") {
+    using namespace efengine;
+    resources::ResourceManager rm;
+    CHECK(rm.GetTexture("assets/no_existe.png", renderer::ColorSpace::sRGB) == nullptr);
+}
