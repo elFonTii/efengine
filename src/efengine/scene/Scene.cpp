c97cce9fb5a015b30db31b0ed23a976b4494a779
= <feluubotta685@gmail.com>
2026-07-18 13:16:45 -0300

    scene/Scene - comentarios

--- src/efengine/scene/Scene.cpp
+++ src/efengine/scene/Scene.cpp
@@ -4,7 +4,7 @@
 
 namespace efengine {
 namespace scene {
-
+    // ADDITIONS
     u32 Scene::Add(SceneObject object) {
         const u32 handle = static_cast<u32>(m_objects.size());
         m_objects.push_back(std::move(object));
@@ -18,19 +18,17 @@ namespace scene {
         return handle;
     }
 
-
+    // GETTERS
     SceneObject& Scene::Get(u32 handle) {
         EF_ASSERT(handle < m_objects.size(), "Scene::Get: Se intenta acceder a un índice fuera de rango");
 
         return m_objects[handle];
     }
-
     const SceneObject& Scene::Get(u32 handle) const { 
         EF_ASSERT(handle < m_objects.size(), "Scene::Get: Se intenta acceder a un índice fuera de rango (const)");
 
         return m_objects[handle];
     }
-
     Light& Scene::GetLight(u32 handle) {
         EF_ASSERT(handle < m_lights.size(), "Scene::GetLight: Se intenta acceder a un índice fuera de rango");
 
