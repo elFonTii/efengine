dab32bf2de11ff103732231eb32454f034ee3ebd
= <feluubotta685@gmail.com>
2026-07-18 13:09:11 -0300

    scene/Scene - API de luces

--- src/efengine/scene/Scene.h
+++ src/efengine/scene/Scene.h
@@ -2,23 +2,33 @@
 
 #include <efengine/core/Types.h>
 #include <efengine/scene/SceneObject.h>
+#include <efengine/renderer/PointLight.h>
+
 
 #include <vector>
-
 namespace efengine{
 namespace scene{
+    using Light = renderer::PointLight;
+
     class Scene{
         public:
             u32 Add(SceneObject object);
+            u32 AddLight(Light light);
 
             SceneObject& Get(u32 handle);
             const SceneObject& Get(u32 handle) const; 
+
+            Light& GetLight(u32 handle);
+            const Light& GetLight(u32 handle) const;
+
+            
 
             // Para iterar y dibujar
             const std::vector<SceneObject>& objects() const { return m_objects; }
 
         private:
             std::vector<SceneObject> m_objects;
+            std::vector<Light> m_lights;
     };
 }
 }
